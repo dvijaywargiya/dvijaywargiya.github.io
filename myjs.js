90056c0fb61d7817317955ff9593c17702ec29f3
dvijaywargiya <dvijaywargiya@gmail.com>
2018-12-05 15:40:23 +0530

    First time

---
+++ myjs.js
@@ -0,0 +1,42 @@
+function mouseOver1() {
+    document.getElementById("athletee").style.display = "block";
+}
+
+function mouseOut1() {
+    document.getElementById("athletee").style.display = "none";
+}
+
+function mouseOver2() {
+    document.getElementById("gamerr").style.display = "block";
+}
+
+function mouseOut2() {
+    document.getElementById("gamerr").style.display = "none";
+}
+
+function mouseOver3() {
+    document.getElementById("sportss").style.display = "block";
+}
+
+function mouseOut3() {
+    document.getElementById("sportss").style.display = "none";
+}
+
+function mouseOver4() {
+    document.getElementById("quoraa").style.display = "block";
+}
+
+function mouseOut4() {
+    document.getElementById("quoraa").style.display = "none";
+}
+
+function mouseOver5() {
+    document.getElementById("dipsitee").style.display = "block";
+}
+
+function mouseOut5() {
+    document.getElementById("dipsitee").style.display = "none";
+}
+
+
+
